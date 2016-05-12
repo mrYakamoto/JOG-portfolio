cfcc3244860bcc217df483bc1071d360fcfb0663
Gregory Wehmeier <wehmenstein@gmail.com>
2016-05-12 12:57:48 -0500

    add s3 bucket info to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_attached_file :picture,
+  :storage => :s3,
   :bucket => 'jason-pictures',
-  :s3_protocol => :https
+  :s3_protocol => :https,
+  :s3_host_name => 's3-us-west-2.amazonaws.com'
 
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
   validates_uniqueness_of :username
+
+
 end
